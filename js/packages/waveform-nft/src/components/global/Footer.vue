3fb542ce52e170682fb22b37eb41db8fe611d561
Alex Brik <mr.alex.brik@gmail.com>
2021-10-25 18:27:08 +0300

    update footer links

--- js/packages/waveform-nft/src/components/global/Footer.vue
+++ js/packages/waveform-nft/src/components/global/Footer.vue
@@ -1,12 +1,13 @@
 <template>
   <footer class="p-0">
-    <div class="flex justify-center items-center text-center text-lg text-ternary-dark dark:text-ternary-light">
+    <div class="flex flex-wrap-reverse justify-center items-center text-center text-lg text-ternary-dark dark:text-ternary-light">
       <div class="mr-5">
         &copy; 2021 WaveTheVoice
       </div>
-      <div class="underline">
-        <a class="mr-5" href="https://t.me/WaveTheVoiceNFT" target="_blank">Telegram</a>
-        <a href="https://twitter.com/WaveTheVoiceNFT" target="_blank">Twitter</a>
+      <div class="underline flex flex-wrap">
+        <a class="mr-5" href="https://twitter.com/WaveTheVoiceNFT" target="_blank">Twitter</a>
+        <a class="mr-5" href="https://wavethevoice.art/discord" target="_blank">Discord</a>
+        <a href="https://t.me/WaveTheVoiceNFT" target="_blank">Telegram</a>
       </div>
     </div>
   </footer>
